4c39fa2f20acaf51c76c659c9c3432118582f07e
igor007_cyber <igor.ramalho007@gmail.com>
2023-08-20 23:28:05 -0300

    Scripts de vida, morte, movimentação da camara

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour
 {
     public Animator anim;
     public float speed;
+    public float vida = 5, tempoMax = 1f, tempoMin, Cronometro; /*Na variavel tempo Max coloque o tempo por segundo
+ que seu player vai tomar dano, Na variavel vida coloque a vida do player */
+    public bool dano; //variaveis publicas bool.    
 
     // Update is called once per frame
     void Update()
@@ -17,5 +20,40 @@ public class Player : MonoBehaviour
         anim.SetFloat("Speed", movement.magnitude);
 
         transform.position = transform.position + movement * speed * Time.deltaTime;
+
+        if (dano == true)
+        {
+            LevarDano();
+        }
+        /*essa condição vai fazer o player morrer quando a vida dele for menor que 10*/
+        if (vida < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*Essa void vai dizer se o inimigo entrou na colisão do player*/
+    void OnCollisionEnter2D(Collision2D C)
+    {
+        if (C.gameObject.tag == "Inimigo")
+        {
+            dano = true;
+        }
+    }
+    /*Essa void vai dizer se o inimigo saio da colisão do player*/
+    void OnCollisionExit2D(Collision2D CE)
+    {
+        if (CE.gameObject.tag == "Inimigo")
+            dano = false;
+    }
+
+    void LevarDano()
+    { /*Essa void vai fazer o player tomar o dano. talvez se alguem tiver duvida eu trago um tutorial explicando como
+ ela funciona*/
+        Cronometro += Time.deltaTime;
+        if (tempoMin < Cronometro)
+        {
+            tempoMin += tempoMax;
+            vida -= 1;
+        }
     }
 }
